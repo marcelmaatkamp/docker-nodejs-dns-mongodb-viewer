63bc8263a3911194d0ebd699d7806747e12c8d52
Marcel Maatkamp <m.maatkamp@gmail.com>
2015-01-14 21:28:19 +0100

    more models

--- dnsViewer/api/models/Address.js
+++ dnsViewer/api/models/Address.js
@@ -1,5 +1,5 @@
 /**
-* Dns.js
+* Address.js
 *
 * @description :: TODO: You might write a short summary of how this model works and what it represents here.
 * @docs        :: http://sailsjs.org/#!documentation/models
@@ -8,15 +8,10 @@
 module.exports = {
 
   attributes: {
-    header: { 
-      model: "Header"
-    },
-    question: { 
-      model: "Question"
-    },
-    address: { 
-      model: "Address"
-    }
+    address : "String",
+    family : "String",
+    port : "integer",
+    size : "integer"
   }
 };
 
--- dnsViewer/api/models/Header.js
+++ dnsViewer/api/models/Header.js
@@ -6,9 +6,18 @@
 */
 
 module.exports = {
-
   attributes: {
-    
+ // id : "integer",
+    qr : "integer",
+    opcode : "integer",
+    aa : "integer",
+    tc : "integer",
+    rd : "integer",
+    ra : "integer",
+    res1 : "integer",
+    res2 : "integer",
+    res3 : "integer",
+    rcode : "integer"
   }
 };
 
